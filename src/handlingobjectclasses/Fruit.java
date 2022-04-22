61604b261e1d802bfcca091a114a2f824d428682
Duvan Leal <duvanleal65@gmail.com>
2022-04-21 23:58:30 -0500

    cambios clase fruta
    se añaden los parámetros y métodos de la clase

--- src/handlingobjectclasses/Fruit.java
+++ src/handlingobjectclasses/Fruit.java
@@ -5,10 +5,51 @@
  */
 package handlingobjectclasses;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Duvan Leal
  */
 public class Fruit {
+    /** Representa la clase Fruit y sus caracteristicas
+ *
+ * @author Duvan leal
+ */
+
+    
+    /**
+     * Representa el nombre de la fruta
+     */
+    public String name;
+    /**
+     * Representa el peso promedio de la fruta
+     */
+    private float averageWeight;
+    /**
+     * Representa una lista de colores de la fruta
+     */
+    public ArrayList<String> colors;
+    
+    /**
+     * Representa el contructor , inicializar las variables 
+     */
+    public Fruit(){
+        
+    }
+    /**
+     * Representa el metodo de obtener colores 
+     * @param colors
+     */ 
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+    /**
+     * Representa el metodo de capturar o guardar colores 
+     * @param colors
+     */ 
+    public void setColors(ArrayList<String> colors) {
+        this.colors = colors;
+    }
     
 }
